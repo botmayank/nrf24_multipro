ed474240dd9a7330d72cff83de7c7a1feec63995
crearo <rishabh.bhardwaj@tonboimaging.com>
2018-09-15 02:46:43 +0530

    Set physical dimension for aruco markers

--- vision/aruco/aruco_finder.py
+++ vision/aruco/aruco_finder.py
@@ -11,10 +11,13 @@ def run_camera_and_detect(cap, camera_matrix, dist_matrix):
         corners, ids, _ = aruco.detectMarkers(gray, dictionary)
 
         if ids is not None and len(ids) > 0:
-            rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, 0.05, camera_matrix, dist_matrix)
+            rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, 0.015, camera_matrix, dist_matrix)
 
-            aruco.drawAxis(gray, camera_matrix, dist_matrix, rvec[0], tvec[0], 0.1)
+            aruco.drawAxis(gray, camera_matrix, dist_matrix, rvec[0], tvec[0], 0.02)
             aruco.drawDetectedMarkers(gray, corners, ids)
+            stride = abs(corners[0][0][0][0] - corners[0][0][1][0])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(gray, "Length: %f" % stride, (0, 50), font, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
         cv2.imshow('frame', gray)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -33,10 +36,12 @@ def setup_camera(width=640, height=480, frame_rate=30):
     cap.set(cv2.CAP_PROP_FPS, frame_rate)
 
     # todo read these values from the camera calibration
-    camera_matrix = np.array([[500., 0., 320.],
-                              [0., 500., 240.],
+    camera_matrix = np.array([[7.2751531880861648e+02, 0., 3.3573062809603971e+02],
+                              [0., 7.2902821773214896e+02, 2.2879353141911656e+02],
                               [0., 0., 1.]])
-    dist_matrix = np.array([0., 0., 0., 0., 0.])
+    dist_matrix = np.array([4.8412257817011632e-02, -1.2834569362402684e-01,
+                            -2.6153996114361634e-03, 5.9434689889416460e-03,
+                            -1.5490960578187754e+00])
 
     return cap, camera_matrix, dist_matrix
 
