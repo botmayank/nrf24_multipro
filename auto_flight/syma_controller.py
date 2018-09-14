21f92a84e9b8f99852496ceec1cc5e0c5f6fe802
mayank.joneja@tonboimaging.com <mayank.joneja@tonboimaging.com>
2018-09-15 02:37:34 +0530

    Added optional delta args to exposed flight methods in SymaController

--- auto_flight/syma_controller.py
+++ auto_flight/syma_controller.py
@@ -46,6 +46,10 @@ class SymaController:
         # close it again so it can be reopened the next time it is run.    
         self.arduino.close()
 
+    def init_throttle(self):
+        self.max_throttle()
+        time.sleep(0.5)
+        self.go_throttle()
 
     def _clamp_input(self, n):
         return max(INPUT_MIN, min(n, INPUT_MAX))
@@ -63,29 +67,31 @@ class SymaController:
         elif input_type == "yaw":
             self.rudder = self._clamp_input(temp)
 
-    def roll(self, dir):
+    def roll(self, dir, delta=ag):
         if dir == "right":
-            self._update_input("roll", self.aileron, +ag)
+            self._update_input("roll", self.aileron, +delta)
         elif dir == "left":
-            self._update_input("roll", self.aileron, -ag)
+            self._update_input("roll", self.aileron, -delta)
 
-    def pitch(self, dir):
+    def pitch(self, dir, delta=eg):
         if dir == "forward":
-            self._update_input("pitch", self.elevator, +eg)
+            self._update_input("pitch", self.elevator, +delta)
         elif dir == "back":
-            self._update_input("pitch", self.elevator, -eg)
+            self._update_input("pitch", self.elevator, -delta)
 
-    def yaw(self, dir):
+    def yaw(self, dir, delta=rg):
         if dir == "ccw":
-            self._update_input("yaw", self.rudder, -rg)
+            self._update_input("yaw", self.rudder, -delta)
         elif dir == "cw":
-            self._update_input("yaw", self.rudder, +rg)
+            self._update_input("yaw", self.rudder, +delta)
 
-    def thrust(self, dir):
+    def thrust(self, dir, delta=tg):
         if dir == "up":
-            self._update_input("thrust", self.throttle, +tg)
+            self._update_input("thrust", self.throttle, +delta)
         elif dir == "down":
-            self._update_input("thrust", self.throttle, -tg)
+            self._update_input("thrust", self.throttle, -delta)
+        elif dir == "mid":
+            self.throttle = INPUT_MID
 
     def reset_inputs(self):
         self.throttle = INPUT_MIN
@@ -106,8 +112,5 @@ class SymaController:
 
     def send_command(self):
         command = "%i, %i, %i, %i" %(self.throttle, self.aileron, self.elevator, self.rudder)
-        # string commands to the Arduino are prefaced with  [PC]
-        # screen.addstr(4, 0, "[PC]: Thr, Roll, Pitch, Yaw:" )
-        # screen.addstr(5, 0, "[PC]: " + command )
         self.arduino.write(command + "\n")
         return command
