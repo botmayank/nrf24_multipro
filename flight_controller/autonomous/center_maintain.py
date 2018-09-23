00cfcfc1e39adf8f6064f63d577f4a0786fd4c60
crearo <rishabh.bhardwaj@tonboimaging.com>
2018-09-23 14:00:36 +0530

    Add initial try at center maintain
    This doesn't work as easyily as I thought it would.
    
    Realized I'll need a lot of black chart paper + make a controller and
    figure out what to do one by one

---
+++ flight_controller/autonomous/center_maintain.py
@@ -0,0 +1,65 @@
+import time
+
+from flight_controller.syma_controller import SymaController
+from vision.aruco.aruco_finder import ArucoFinder
+from vision.aruco.pose import Pose
+
+syma = None
+aruco_finder = None
+
+PORT = "/dev/ttyUSB0"
+
+
+def get_pose():
+    return aruco_finder.run_camera_and_detect(True, syma.values())
+
+
+def control():
+    delta = 0.3
+    while True:
+        pose = get_pose()
+        if pose is not None:
+            syma.pitch(delta if pose.px > 20 else -delta if pose.px < -20 else 0)
+            syma.roll(delta if pose.py > 20 else -delta if pose.py < -20 else 0)
+
+            if pose.yaw != Pose.UNKNOWN:
+                if 100 < pose.yaw <= 270:
+                    syma.yaw(-delta)
+                elif 80 > pose.yaw >= 0 or 270 < pose.yaw <= 360:
+                    syma.yaw(delta)
+                else:
+                    syma.yaw(0)
+
+            if pose.dist_z == Pose.UNKNOWN:
+                syma.throttle = 1530
+            elif pose.dist_z >= 150:
+                syma.delta_thrust_relative(delta)
+            elif pose.dist_z <= 130:
+                syma.delta_thrust_relative(-delta)
+            else:
+                syma.level_throttle()
+        else:
+            syma.reset_rotation()
+            syma.level_throttle()
+
+        print(syma.values())
+        syma.send_command()
+
+
+if __name__ == '__main__':
+    try:
+        syma = SymaController(PORT)
+        time.sleep(2)
+
+        syma.go_throttle()
+        time.sleep(1)
+
+        aruco_finder = ArucoFinder()
+
+        control()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Cleanly exit
+        del syma
+        del aruco_finder
